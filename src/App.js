351ee8d50a5183c5501159acfec31225190a4f97
EdyFS <157139859+EdyFS@users.noreply.github.com>
2024-10-19 09:45:10 -0300

    Add files via upload
    Pastas

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import Novo from './Novo';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <p>Olá sala</p>
+    </div>
+   
+  );
+}
+
+export default App;
---
+++ src/Novo.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Novo() {
+  return (
+    <div>
+      <p>Olá sala Novo</p>
+    </div> 
+  );
+}
+
+export default Novo;
